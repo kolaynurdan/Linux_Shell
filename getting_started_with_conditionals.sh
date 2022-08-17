1e66891aa730b782169743c0772c7b0465135a10
nurdan <kolaynurdan@gmail.com>
2022-08-17 14:09:01 +0300

    Update getting_started_with_conditionals.sh

--- getting_started_with_conditionals.sh
+++ getting_started_with_conditionals.sh
@@ -1,12 +1,11 @@
 #Read in one character from STDIN.
 #If the character is 'Y' or 'y' display "YES".
 #If the character is 'N' or 'n' display "NO".
-#!/bin/bash
-read word
-if [[($word == 'y') || ($word == 'Y')]]
+read x
+if [[($x == 'y') || ($x == 'Y')]]
 then
     echo "YES"
-        elif [[($word == 'n') || ($word == 'N')]]
+        elif [[($x == 'n') || ($x == 'N')]]
         then
         echo "NO"
 fi
