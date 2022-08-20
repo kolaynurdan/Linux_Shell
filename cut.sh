aa0f661a1f4bb33c36c32142ed09d51adf7adb55
nurdan <kolaynurdan@gmail.com>
2022-08-20 18:10:52 +0300

    Update cut.sh

--- cut.sh
+++ cut.sh
@@ -1,2 +1,7 @@
 # print the 3rd character from each line as a new line of output.
 cut -c 3
+#print the 2nd and 7th character from each line as a new line of output.
+while read line
+do
+    echo ${line} | cut -c 2,7
+done
