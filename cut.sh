4dd042e7c1d62810217695fc1b189ed04532270b
nurdan <kolaynurdan@gmail.com>
2022-08-20 18:52:10 +0300

    Update cut.sh

--- cut.sh
+++ cut.sh
@@ -24,3 +24,5 @@ while read line
 do
     echo $line | cut -d ' ' -f 4
 done
+#The output should contain N lines. For each input sentence, identify and display its first three words. Assume that the space (' ') is the only delimiter between words. 
+cut -d " " -f 1-3
