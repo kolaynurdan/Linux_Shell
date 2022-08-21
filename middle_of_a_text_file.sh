b998a9e2fd22e45c605a8b5d05d7b8d7b8124e2a
nurdan <kolaynurdan@gmail.com>
2022-08-21 14:10:03 +0300

    Create middle_of_a_text_file.sh

---
+++ middle_of_a_text_file.sh
@@ -0,0 +1,6 @@
+#Display the lines (from line number 12 to 22, both inclusive) for the input file.
+head -n 22 | tail -n 11
+#Output the last 20 lines of the text file
+tail -n 20
+#Output the last 20 characters of the text file
+tail -c 20
