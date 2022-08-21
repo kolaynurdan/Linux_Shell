ccf8c7dff32be3e929b265d1443a8d365517b588
nurdan <kolaynurdan@gmail.com>
2022-08-21 16:42:06 +0300

    Create awk_command.sh

---
+++ awk_command.sh
@@ -0,0 +1,4 @@
+#For each row of data, append a space, a colon, followed by another space, and the grade. Observe the format showed in the sample output.
+awk '{ total=$2+$3+$4; avg=total/3; print $0 " : " (avg > 50 ? avg > 60 ? avg > 80 ? "A" : "B" : "C" : "FAIL"); }'
+#Concatenate every 2 lines of input with a semi-colon.
+awk 'END{ if((NR%2)) print p ";" }!(NR%2){ print p ";" $0 }{ p = $0 }'
