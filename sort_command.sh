2f1b4f6d57ac4294e98e2f28e73a630c9bb03ee3
nurdan <kolaynurdan@gmail.com>
2022-08-21 14:46:17 +0300

    Update sort_command.sh

--- sort_command.sh
+++ sort_command.sh
@@ -1,2 +1,12 @@
 #Output the text file with the lines reordered in lexicographical order.
 sort
+#Output the text file with the lines reordered in reverse lexicographical order.
+sort -r
+#Output the text file with the lines reordered in numerically ascending order.
+sort -n
+#The text file, with lines re-ordered in descending order (numerically).
+sort -nr
+#Rearrange the rows of the table in descending order of the values for the average temperature in January (i.e, the mean temperature value provided in the second column).
+sort -n -k2 -r -t $'\t'
+#The data has been sorted in ascending order of the average monthly temperature in January (i.e, the second column).
+sort -t$'\t' -n -k2
