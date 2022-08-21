e451401f042f6bcfb0b37dcd98fb7a8c5f61813f
nurdan <kolaynurdan@gmail.com>
2022-08-21 16:26:27 +0300

    Update arrays.sh

--- arrays.sh
+++ arrays.sh
@@ -19,3 +19,7 @@ echo ${#array[@]}
 #Transform the names as described and display the entire array of country names with a space between each of them.
 array=($(cat))
 echo ${array[@]/[:A-Z:]/.}
+#There are N integers in an array A. All but one integer occur in pairs. Your task is to find the number that occurs only once
+read
+arr=($(cat)) 
+echo "${arr[@]}" | tr ' ' '\n' |sort | uniq -u | tr '\n' ' '
