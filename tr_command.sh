742b976eebcdc3de8b7158dbf5b17b4dd9439ffb
nurdan <kolaynurdan@gmail.com>
2022-08-21 14:25:15 +0300

    Update tr_command.sh

--- tr_command.sh
+++ tr_command.sh
@@ -1,2 +1,7 @@
 #Output the text with all parentheses () replaced with box brackets [].
 tr '()' '[]'
+#In a given fragment of text, delete all the lowercase characters a - z.
+tr -d "a-z"
+#Replace all sequences of multiple spaces with just one space.
+tr -s ' '
+
